9725ce66af89dc4584b11022f8a587933cd16181
sarwangoutam2004-dev <goutam2004@gmail.com>
2026-08-02 20:33:25 +0530

    add task functionality

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
 
+    'todo',   # 👈 app ka naam alag line me
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- todo_main/views.py
+++ todo_main/views.py
@@ -1,6 +1,11 @@
 
 from django.shortcuts import render
+from todo.models import Task
 
 def home(request):
+    tasks=Task.objects.all().order_by('-updated_at')
+    context={
+        'tasks': tasks,
+    }
 
-    return render (request,'home.html')
+    return render (request,'home.html',context)
