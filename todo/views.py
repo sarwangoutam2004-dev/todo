b8a021e3e598d17702d85d2eb5b81b537ce0f6d4
sarwangoutam2004-dev <goutam2004@gmail.com>
2026-08-04 22:43:52 +0530

    todo app final

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from .models import Task
 
-
 def addTask(request):
-    task=request.POST['task']
+    task = request.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
 
-    return redirect('home') 
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed=True
+    task.save()
+    return redirect('home')
 
-# Create your views here.
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+
+    if request.method == "POST":
+        task.task = request.POST['task']
+        task.save()
+        return redirect('home')
+
+    return render(request, 'edit_task.html', {'task': task})
+from django.shortcuts import get_object_or_404
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -4,6 +4,8 @@ from todo.models import Task
 
 def home(request):
     tasks=Task.objects.all().order_by('-updated_at')
+    completed_task=Task.objects.filter(is_completed=True)
+    print(completed_task)
     context={
         'tasks': tasks,
     }
